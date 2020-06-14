29d39027e228209b45770fd5023c51a2933d2d74
Eugene Venev <venevevg@gmail.com>
2020-06-15 03:26:39 +0700

    v 3.9

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -15,8 +15,8 @@ public class Eratosthenes {
 
     private void sift() {
         int i, j;
-        for (i = 1; i < sieve.length ; i++) {
-            for (j = 1; j < sieve.length ; j++) {
+        for (i = 1; i < sieve.length; i++) {
+            for (j = 1; j < sieve.length; j++) {
                 if (i * j <= 3) continue;
 
                 while ((sieve.length - 1) >= i * j) {
@@ -27,9 +27,14 @@ public class Eratosthenes {
         }
     }
 
+    public boolean isSimple(int n){
+        return this.sieve[sieve.length-1];
+    }
+
+
     public static void main(String[] args) {
         Eratosthenes era = new Eratosthenes(20);
-        System.out.println(Arrays.toString(era));
+        System.out.println(era.isSimple(7));
     }
 }
 
