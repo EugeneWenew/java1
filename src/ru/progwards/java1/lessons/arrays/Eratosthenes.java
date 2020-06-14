77c60dd6587023539f29797cc36e5778ce490404
Eugene Venev <venevevg@gmail.com>
2020-06-14 23:25:04 +0700

    v 3.8

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -1,38 +1,35 @@
 package ru.progwards.java1.lessons.arrays;
 
-import java.time.chrono.Era;
 import java.util.Arrays;
 
 public class Eratosthenes {
     int N;
-    boolean[] sieve = new boolean[N];
-
-
-    public static void main(String[] args) {
-        Eratosthenes era = new Eratosthenes(50);
-        System.out.println(era);
-    }
-
-    public Eratosthenes() {
-    }
+    Boolean[] sieve = new Boolean[N];
 
     public Eratosthenes(int N) {
         this.N = N;
-        boolean[] sieve = new boolean[N];
+        sieve = new Boolean[N];
         Arrays.fill(sieve, true);
         sift();
-
     }
 
     private void sift() {
         int i, j;
-        for (i = 2; i < N - 1; i++) {
-            for (j = 2; j < N - 1; j++) {
-                while ((N - 1) >= i * j) {
+        for (i = 1; i < sieve.length - 1; i++) {
+            for (j = 1; j < sieve.length - 1; j++) {
+                if (i * j <= 3) continue;
+                ;
+                while ((sieve.length - 1) >= i * j) {
                     sieve[j * i] = false;
+                    j++;
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Eratosthenes era = new Eratosthenes(20);
+        System.out.println(Arrays.toString(era));
     }
 }
 
