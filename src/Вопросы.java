86e2c3e2d9cde7d3371626a96dc2f57db586875e
Eugene Venev <venevevg@gmail.com>
2020-06-16 03:12:33 +0700

    Вопросы

---
+++ src/Вопросы.java
@@ -0,0 +1,61 @@
+public class Вопросы {
+    public static metod1() {
+                                                    //передача переменных из статика в нестатик
+                                                    //вызов статика из других методов
+    }
+
+    public metod2() {
+                                                    //тоже
+                                                    //тоже
+    }
+
+
+    public static nekotoryjMetod_1() {
+
+    }
+
+
+    public nekotoryjMetod_2() {
+
+    }
+}
+
+
+
+
+
+
+
+
+public class primerConstructora {
+    int N;                                              //поле класса / N - свойство
+
+    primerConstructora(int N) {
+    this.N=                                             //инициализация N в конструкторе / физический смысл
+    N=
+    }
+
+    public metodConstructora() {
+    this.N                                              //вызов переменной N в методе
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public void metodVoid() {
+                                                        //вызов метода void в другом методе
+    }
+
+    public static void main(String[] args) {
+        .metodVoid();                                   //как вызывается void/ не void метод
+
+
+         }
+}
