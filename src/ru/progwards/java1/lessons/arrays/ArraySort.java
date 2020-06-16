5651a26fa6ebbc46b18bbe84c41f3bb7c886dfe7
Eugene Venev <venevevg@gmail.com>
2020-06-16 14:23:07 +0700

    3.9

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -3,20 +3,21 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class ArraySort {
-    int N;
-    int[] sortArray;
+    Integer N;
+    Integer[] sortArray;
 
     public ArraySort(int N) {
         this.N = N;
-        this.sortArray = new int[N];
+        this.sortArray = new Integer[N];
     }
 
 
     public static void sort(int[] a) {
         ArraySort stAr = new ArraySort(10);
-        int i, j;
-        for (i = 1; i < stAr; i++) {
-            for (j = 1; j < stAr; j++) {
+
+        for (Integer i = 1; i < sortArray; i++) {
+            for (Integer j = 1; j < stAr; j++) {
+
             }
         }
     }
