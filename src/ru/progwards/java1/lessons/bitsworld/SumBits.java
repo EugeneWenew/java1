acbd84de765dc809498bc462e7c4b3a93bef71d9
Eugene Venev <venevevg@gmail.com>
2020-06-21 05:56:04 +0700

    алгоритм рабочий

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -1,19 +1,27 @@
 package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
-    public static int sumBits(byte value) {
-        int result = 0;
-        int intermediate = 0;
+    public static int sumBits(byte value) {     //переменная byte
+        int result = 0;                         //инициализация результата
+        if (value > 0) {
+            while (value > 0) {                     //цикл прогона всего числа
+                result += (byte) (value & 0b1);      //выделение правого БИТА переменной value c инкрементом результата
+                value = ((byte) (value >> 1));      //сдвиг вправо с заполнением нулями
+            }
+            return result;
+        } else if (value < 0) {
+            for (int i = 0; i < 8; i++) {
+                result += (byte) (value & 0b1);
+                value = ((byte) (value >> 1));
+            }
+            return result;
+        } else return 0;
 
-        while (value > 0) {
-            result += (value & 0b00000001);
-            value = (byte) (value >> 1);
 
-        }
-        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte) 7));
+        byte value = (byte) 0b10000000;
+        System.out.println(sumBits(value));     //переменная byte
     }
 }
