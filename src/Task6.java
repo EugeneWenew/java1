f1519612424fc8b35f8ca1b139e6a6e85e71e4dd
Eugene Venev <venevevg@gmail.com>
2020-06-21 04:13:40 +0700

    алгоритм рабочий

--- src/Task6.java
+++ src/Task6.java
@@ -22,13 +22,13 @@ import java.util.Arrays;
 public class Task6 {
     public static void main(String[] args) {
 
-        int a[] = {11, 22, 33};
-        Task6 m1 = new Task6();
-        System.out.println(m1.sumArrayItems(a));
-        System.out.println();
-        Rectangle rec1 = new Rectangle(5, 6);
-        Rectangle rec2 = new Rectangle(7, 8);
-        System.out.println(rec1.compareTo(rec2));
+//        int a[] = {11, 22, 33};
+//        Task6 m1 = new Task6();
+//        System.out.println(m1.sumArrayItems(a));
+//        System.out.println();
+//        Rectangle rec1 = new Rectangle(5, 6);
+//        Rectangle rec2 = new Rectangle(7, 8);
+//        System.out.println(rec1.compareTo(rec2));
 
 //        int[] a1 = {1, 1, 1, 1, 3};
 //        int[] a2 = new int[5];
@@ -40,13 +40,50 @@ public class Task6 {
 //        int[] a2 = Arrays.copyOf(a1, a1.length);
 //        Arrays.sort(a2);
 //        System.out.println(Arrays.equals(a1, a2));
+//
+//        int[] a1 = {12, 5, 0, 58, 36};
+//        int[] a2 = Arrays.copyOf(a1, a1.length);
+//        a2[2] = 0;
+//        System.out.println(Arrays.equals(a1, a2));
 
-        int[] a1 = {12, 5, 0, 58, 36};
-        int[] a2 = Arrays.copyOf(a1, a1.length);
-        a2[2] = 0;
-        System.out.println(Arrays.equals(a1, a2));
+//
+//        String str1 = new String("12345");
+//        String str2 = new String("12345");
+//        System.out.println(str1.equals(str2));
 
 
+//
+//
+//        Integer a = 1;
+//        Integer b = 5;
+//        System.out.println(a.compareTo(b));
+//
+
+
+//
+//        String str1 = "12345";
+//        String str2 = "12345";
+//        System.out.println(str1 == str2);
+////
+
+        Integer a = 1;
+        Integer b = 1;
+        System.out.println(a.compareTo(b));
+
+//
+//
+//
+//        String str1 = new String("12345");
+//        String str2 = new String("12345");
+//        System.out.println(str1 == str2);
+//
+//
+//
+//
+//        Integer a = 5;
+//        Integer b = 1;
+//        System.out.println(a.compareTo(b));
+//
 
     }
     public int arrayMax(int[] a){
---
+++ src/Task7.java
@@ -0,0 +1,43 @@
+public class Task7 {
+    Grade value;
+
+    public static void main(String[] args) {
+//        System.out.println(1 & 0);
+//        System.out.println(1 | 0);
+//        System.out.println(1 & 1);
+//        System.out.println(~1);         // 0
+//        System.out.println(1 | 1);
+        System.out.println(intToGrade(5));
+
+        //  int result = value & 0b1;
+
+
+    }
+
+    enum Grade {
+        VERYBAD, BAD, SATISFACTORILY, GOOD, EXCELLENT, NOTDEFINED
+    }
+
+    static Grade intToGrade(int grade) {
+        switch (grade) {
+            case 1:
+                return Grade.VERYBAD;
+            case 2:
+                return Grade.BAD;
+            case 3:
+                return Grade.SATISFACTORILY;
+            case 4:
+                return Grade.GOOD;
+            case 5:
+                return Grade.EXCELLENT;
+            default:
+                return Grade.NOTDEFINED;
+        }
+
+
+    }
+
+}
+
+
+
