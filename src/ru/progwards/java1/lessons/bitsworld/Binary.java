f223fad1607b33b917f423b2cbf5d3bf10031c1a
Eugene Venev <venevevg@gmail.com>
2020-06-21 16:30:34 +0700

    --

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -9,12 +9,30 @@ public class Binary {
     }
 
     public String toString() {
-        if (num==0) return "00000000";
+        if (num == 0) return "00000000";
         String value = "";
         int result = num;
+        int counter = 0;
         while (result > 0) {
             value += result % 2;
             result = result / 2;
+            counter++;
+        }
+        switch (counter) {
+            case 1:
+                return "0000000" + value;
+            case 2:
+                return "000000" + value;
+            case 3:
+                return "00000" + value;
+            case 4:
+                return "0000" + value;
+            case 5:
+                return "000" + value;
+            case 6:
+                return "00" + value;
+            case 7:
+                return "0" + value;
         }
 
 
@@ -22,7 +40,7 @@ public class Binary {
     }
 
     public static void main(String[] args) {
-        Binary VAL = new Binary((byte) 0);
+        Binary VAL = new Binary((byte) 3);
         System.out.println(VAL.toString());
     }
 }
