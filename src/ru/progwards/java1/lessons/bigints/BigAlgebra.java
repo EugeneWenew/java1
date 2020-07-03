5b9fac088a02938a7d40eeaf75f1269d1cccae42
Eugene Venev <venevevg@gmail.com>
2020-07-04 04:02:55 +0700

    9.1

--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -6,12 +6,12 @@ import java.math.BigDecimal;
 
 public class BigAlgebra {
     BigDecimal fastPow(BigDecimal num, int pow) {
-        if (pow == 0) return BigDecimal.valueOf(1.);        //проверка нулевой степени
-        String BYNPOW = Integer.toString(pow, 2);       //представление степени двоичным числом
-        BigDecimal res = BigDecimal.valueOf(0);             //инициализация результата НУЛЕМ
-        for (int i = BYNPOW.length() - 1; i <= 0; i++) {     //цикл перебора от СТАРШЕГО бита к МЛАДШЕМУ
-            if (BYNPOW[i] == 1) {
-                res = res.multiply(res).multiply(num);
+        if (pow == 0) return BigDecimal.valueOf(1);        //проверка нулевой степени
+        String BYNPOW = Integer.toString(pow, 2);      //представление степени двоичным числом
+        BigDecimal res = (num);             //инициализация результата НУЛЕМ
+        for (int i = BYNPOW.length() - 1; i >= 0; i--) {    //цикл перебора от СТАРШЕГО бита к МЛАДШЕМУ
+            if (BYNPOW.charAt(i) == '1') {                  //считывание значения бита
+                res = (res.multiply(res)).multiply(num);
             } else res = res.multiply(res);
         }
         return res;
@@ -20,7 +20,7 @@ public class BigAlgebra {
     public static void main(String[] args) {
 
         BigAlgebra cadabra = new BigAlgebra();
-        System.out.println(cadabra.fastPow(BigDecimal.valueOf(3), 5));
+        System.out.println(cadabra.fastPow(BigDecimal.valueOf(2), 1));
 
     }
 }
