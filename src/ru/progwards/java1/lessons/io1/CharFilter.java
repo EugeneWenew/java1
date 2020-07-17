a56091e338ade661df0f01048cbbd5c863ace6fe
Eugene Venev <venevevg@gmail.com>
2020-07-17 18:27:19 +0700

    10.8

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -3,60 +3,52 @@ package ru.progwards.java1.lessons.io1;
 import java.io.*;
 
 public class CharFilter {
-//    public static void filterFile(String inFileName, String outFileName, String filter) {    //throws IOexception
-//        FileReader fileInput = null;
-//        FileWriter fileOutput = null;
-//        try {
-//            fileInput = new FileReader(inFileName);
-//            fileOutput = new FileWriter(outFileName);
-//            char[] charCode = filter.toCharArray();
-//            try {
-//                int ch = fileInput.read();
-////                habkbblbnlnfbuooudgberh
-////                ,./l;'p[]
-//
-//
-//                filter.contains(ch);
-//
-//
-////                // nextline + цикл tochararray
-////                for (int b = 0; b < bytes.length; b++) {
-////                    for (int i = 0; i < charCode.length; i++) {
-////                        if (bytes[b] == charCode[b]) {
-////
-////
-////                            System.arraycopy(bytes, b + 1, bytes, b, bytes.length - b);
-////                            fileOutput.write(i);
-////                        }
-////                    }
-////                }
-////            } finally {
-////                try {
-////                    fileInput.close();
-////                } finally {
-////                    fileOutput.close();
-////                }
-////            }
-////        } finally {
-////            if fileInput !=
-////
-//            }
-//        }
+    public static void filterFile(String inFileName, String outFileName, String filter) {    //throws IOexception
+        FileReader fileInput = null;
+        FileWriter fileOutput = null;
+        try {
+            fileInput = new FileReader(inFileName);
+            fileOutput = new FileWriter(outFileName);
+            char[] code = filter.toCharArray();
+            try {
+                //asdfghjk
+                //  dfg
+                int ch = fileInput.read();
+                for (int i = 0; i < code.length; i++) {                     //  contains ??
+                    if (inFileName.contains(String.valueOf(code[i]))) System.arraycopy(inFileName,i+1,inFileName,i,inFileName.length()-i);                  //  fileInput ??     (filter)             //to String ??
+                    fileOutput.write(inFileName);                                   //.write или arraycopy
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fileInput.close();
+                } finally {
+                    fileOutput.close();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     public static void main(String[] args) {
         String str = new String("123.txt");
         String str2 = new String("456.txt");
         String filter = new String("456.txt");
-
-//        System.out.println(filterFile(str, str2, filter));
-
         int ch = 97;
         char chr = (char) ch;
-        System.out.println(ch);
-        System.out.println(chr);
+//        System.out.println(ch);
+//        System.out.println(chr);
+
+
     }
-
-
 }
 
 
